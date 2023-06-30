9bf14c81e3a3f661ea41951667a3510c8fdba3a1
Lou8is <lis.gpublic@gmail.com>
2023-06-30 11:57:30 +0200

    docs(fr): Translates title suffix

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,2 @@
 export const HOMEPAGE_TITLE = 'Documentation';
-export const TITLE_SUFFIX = ' | NestJS - A progressive Node.js framework';
+export const TITLE_SUFFIX = ' | NestJS - Un framework Node.js progressif';
